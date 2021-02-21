2f897f4ede63d083b102c0753f79095420a0f7d2
multifeb13 <musicfair@gmail.com>
2021-02-21 23:20:07 +0900

    first commit

---
+++ pj_base/Main.cpp
@@ -0,0 +1,21 @@
+#include <cstdint>
+#include <cstdio>
+
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    const uint32_t LED_PIN = 25;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while (true) {
+        printf("Hello, world!\n");
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+    }
+
+    return 0;
+}
